4866f4ed92a36740f64233ef979c79d9603ef9cb
Mfundo Bright Shabalala <mfundoshabalala@gmail.com>
2022-02-18 09:38:54 +0200

    interface update

--- interfaces/index.d.ts
+++ interfaces/index.d.ts
@@ -1,3 +1,37 @@
-interface ChildrenProps {
-	children?: React.ReactNode;
-}
+type FormInputProps = {
+	inputID: string;
+	inputType: string;
+	inputLabel: string;
+	required?: boolean;
+	inputPlaceholder?: string;
+};
+
+type ButtonProps = {
+	buttonType: "button" | "submit" | "reset" | undefined;
+	buttonLabel: string;
+	buttonClass?: string;
+};
+
+type BrandLogoProps = {
+	href: string;
+	src?: string;
+	alt?: string;
+	width?: number;
+	height?: number;
+};
+
+type BrandLogoComponent = React.FunctionComponent<BrandLogoProps>;
+
+type FormInputComponent = React.FunctionComponent<FormInputProps>;
+
+type FormDropdownComponent = React.FunctionComponent;
+
+type FormButtonComponentProps = React.FunctionComponent<ButtonProps>;
+
+type FormPanelComponent = React.FunctionComponent<{ children: React.ReactNode }>;
+
+type FormComponentProps = React.FunctionComponent<{ children: React.ReactNode }> & {
+	Input: FormInputComponent;
+} & { Dropdown: FormDropdownComponent } & { Button: FormButtonComponentProps } & {
+	Panel: FormPanelComponent;
+} & { BrandLogo: BrandLogoComponent };
