392af4293771365f02ae3941ab365ee840cdb3e9
Mfundo Shabalala <mfundoshabalala@gmail.com>
2022-03-15 13:37:58 +0200

    removing unused api pages

--- pages/api/user.ts
+++
@@ -1,8 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-	res.json({
-		name: "John Doe",
-		age: 30,
-	});
-}
