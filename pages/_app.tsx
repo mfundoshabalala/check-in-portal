117fd27f939ae98ff09d14cf1e8d55c43b06f645
Mfundo Bright Shabalala <mfundoshabalala@gmail.com>
2022-06-30 21:52:11 +0200

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,52 +1,29 @@
 // Global styles
 import "../styles/globals.css";
-//
+// package import
 import Head from "next/head";
-import { useEffect, useState } from "react";
 // types import
 import type { AppProps } from "next/app";
-// components import
-import BaseLayout from "layout/BaseLayout";
-import { supabase } from "utils/supabaseClient";
-import updateSupabaseCookie from "utils/updateSupabaseCookie";
-import Router, { useRouter } from "next/router";
+import BasicLayout from "layouts/BasicLayout";
 
 function MyApp({ Component, pageProps }: AppProps) {
-	/*
-		To keep the auth cookie up to date, we have to listen to changes in the authentication state of Supabase.
-		On every change, we have to call the /api/auth endpoint to update the cookie accordingly.
-	*/
-	const [authenticatedState, setAuthenticatedState] = useState("not-authenticated");
-	const router = useRouter();
-	useEffect(() => {
-		const { data: authListener } = supabase.auth.onAuthStateChange((event, session) => {
-			updateSupabaseCookie(event, session);
-
-			if (event === "SIGNED_IN") {
-				setAuthenticatedState("authenticated");
-				Router.push("/auth/dashboard");
-			}
-			if (event === "SIGNED_OUT") setAuthenticatedState("not-authenticated");
-
-			console.log(authenticatedState);
-		});
-
-		// checkUser();
-
-		return () => {
-			authListener?.unsubscribe();
-		};
-	}, [authenticatedState, router]);
-
 	return (
 		<>
 			<Head>
 				<meta charSet='utf-8' />
 				<meta name='viewport' content='width=device-width, initial-scale=1' />
+				<meta
+					name='description'
+					content='This is a platform for students to track their attendance.'
+				/>
+				<meta name='keywords' content='capaciti, attendance, tracker' />
+				<meta name='author' content='Capaciti' />
+				<link rel='icon' href='/favicon.ico' />
+				<title>Capaciti: Candidate Attendance Tracker</title>
 			</Head>
-			<BaseLayout>
+			<BasicLayout>
 				<Component {...pageProps} />
-			</BaseLayout>
+			</BasicLayout>
 		</>
 	);
 }
