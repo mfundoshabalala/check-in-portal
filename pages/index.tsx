dcf3bd8c8326e70729be649fbb13e95da90c6276
Mfundo Bright Shabalala <mfundoshabalala@gmail.com>
2022-06-30 21:52:18 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -39,91 +39,102 @@ const HomePage = () => {
 	}, [formState, isSubmitSuccessful, reset]);
 
 	return (
-		<>
-			<form className='space-y-6 text-center' onSubmit={handleSubmit(onSubmit)}>
-				<BrandLogo href='/' alt='Capaciti Logo' />
-				<h1 className='text-2xl font-black tracking-tight text-center text-slate-800 upper'>
-					Candidate Attendance Tracker
-				</h1>
-				<section className='form-wrapper'>
-					<div className='form-group-container'>
-						<div className='form-group'>
-							<label htmlFor='firstName'>First Name</label>
-							<input
-								type='text'
-								placeholder='John'
-								{...register("firstName")}
-								className={`form-control ${errors.firstName ? "is-invalid" : ""}`}
-							/>
-							<span className='invalid-input'>{errors.firstName?.message}</span>
-						</div>
-						<div className='form-group'>
-							<label htmlFor='lastName'>Last Name</label>
-							<input
-								type='text'
-								placeholder='Doe'
-								{...register("lastName")}
-								className={`form-control ${errors.lastName ? "is-invalid" : ""}`}
-							/>
-							<span className='invalid-input'>{errors.lastName?.message}</span>
-						</div>
-					</div>
-					<div className='form-group-container'>
-						<div className='form-group'>
-							<label htmlFor='candidateNumber'>Candidate Number</label>
-							<input
-								type='text'
-								placeholder='CITI022'
-								{...register("candidateNumber", {
-									pattern: /^[A-Z]{4}[0-9]{3}$/,
-								})}
-								className={`form-control uppercase ${errors.candidateNumber ? "is-invalid" : ""}`}
-							/>
-							<span className='invalid-input'>{errors.candidateNumber?.message}</span>
-						</div>
-						<div className='form-group'>
-							<label htmlFor='cohortName'>Cohort Name</label>
-							<input
-								type='text'
-								placeholder='CitiDev'
-								{...register("cohortName")}
-								className={`form-control ${errors.cohortName ? "is-invalid" : ""}`}
-							/>
-							<span className='invalid-input'>{errors.cohortName?.message}</span>
-						</div>
+		<form onSubmit={handleSubmit(onSubmit)}>
+			<BrandLogo href='/' alt='Capaciti Logo' />
+			<h1 className='text-2xl font-black tracking-tight text-center text-slate-800 upper'>
+				Candidate Attendance Tracker
+			</h1>
+			<section className='form-wrapper'>
+				<div className='form-group-container'>
+					<div className='form-group'>
+						<label className='form-label required' htmlFor='firstName'>
+							First Name
+						</label>
+						<input
+							type='text'
+							placeholder='John'
+							{...register("firstName")}
+							className={`form-control ${errors.firstName ? "is-invalid" : ""}`}
+						/>
+						<span className='invalid-input'>{errors.firstName?.message}</span>
 					</div>
 					<div className='form-group'>
-						<label htmlFor='message'>Message</label>
-						<textarea
-							{...register("message")}
-							placeholder='Reason for lateness/absence'
-							className={`form-control ${errors.message ? "is-invalid" : ""}`}
+						<label className='form-label required' htmlFor='lastName'>
+							Last Name
+						</label>
+						<input
+							type='text'
+							placeholder='Doe'
+							{...register("lastName")}
+							className={`form-control ${errors.lastName ? "is-invalid" : ""}`}
 						/>
-						<span className='invalid-input'>{errors.message?.message}</span>
+						<span className='invalid-input'>{errors.lastName?.message}</span>
 					</div>
-					<div className='btn-group'>
-						<button type='submit' className='btn btn-primary'>
-							Submit
-						</button>
-						<button
-							type='button'
-							className='btn btn-secondary'
-							onClick={() =>
-								reset({
-									firstName: "",
-									lastName: "",
-									candidateNumber: "",
-									cohortName: "",
-									message: "",
-								})
-							}
-						>
-							Reset
-						</button>
+				</div>
+				<div className='form-group-container'>
+					<div className='form-group'>
+						<label className='form-label required' htmlFor='candidateNumber'>
+							Candidate Number
+						</label>
+						<input
+							type='text'
+							placeholder='CITI022'
+							{...register("candidateNumber", {
+								pattern: /^[A-Z]{4}[0-9]{3}$/,
+							})}
+							className={`form-control uppercase ${errors.candidateNumber ? "is-invalid" : ""}`}
+						/>
+						<span className='invalid-input'>{errors.candidateNumber?.message}</span>
 					</div>
-				</section>
-			</form>
-		</>
+					<div className='form-group'>
+						<label className='form-label required' htmlFor='cohortName'>
+							Cohort Name
+						</label>
+						<input
+							type='text'
+							placeholder='CitiDev'
+							{...register("cohortName")}
+							className={`form-control ${errors.cohortName ? "is-invalid" : ""}`}
+						/>
+						<span className='invalid-input'>{errors.cohortName?.message}</span>
+					</div>
+				</div>
+				<div className='form-group'>
+					<label className='form-label' htmlFor='message'>
+						Message
+					</label>
+					<textarea
+						{...register("message")}
+						placeholder='Reason for lateness/absence'
+						className={`form-control ${errors.message ? "is-invalid" : ""}`}
+					/>
+					<span className='invalid-input'>{errors.message?.message}</span>
+				</div>
+				<div className='btn-group'>
+					<button type='submit' className='btn btn-primary'>
+						In
+					</button>
+					<button type='submit' className='btn btn-success'>
+						Out
+					</button>
+					<button
+						type='button'
+						className='btn btn-secondary'
+						onClick={() =>
+							reset({
+								firstName: "",
+								lastName: "",
+								candidateNumber: "",
+								cohortName: "",
+								message: "",
+							})
+						}
+					>
+						Reset
+					</button>
+				</div>
+			</section>
+		</form>
 	);
 };
 
