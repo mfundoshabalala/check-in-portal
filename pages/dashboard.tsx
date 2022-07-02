6d0760f8373d2a3a49e0e794d3ac94f0dcd40844
Mfundo Bright Shabalala <mfundoshabalala@gmail.com>
2022-07-02 19:45:42 +0200

    Update dashboard.tsx

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -2,12 +2,16 @@ import React from "react";
 // components import
 import Sidebar from "components/Sidebar";
 import DashboardLayout from "layouts/DashboardLayout";
+import Navbar from "components/Navbar";
 
 const Dashboard = () => {
 	return (
 		<DashboardLayout>
 			<Sidebar />
-			<section className=''>Content Section</section>
+			<section className='content'>
+				<Navbar />
+				Content Section
+			</section>
 		</DashboardLayout>
 	);
 };
