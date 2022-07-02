3c35aa9baa6162290c16b4f0c7ba4c3d06db312f
Mfundo Bright Shabalala <mfundoshabalala@gmail.com>
2022-07-02 21:08:33 +0200

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 
 const Navbar = () => {
@@ -6,6 +7,18 @@ const Navbar = () => {
 			<div className='search'>
 				<input type='search' value='' placeholder='Search...' />
 				<i className='fa fa-search' />
+			</div>
+			<div className='flex items-center gap-x-12'>
+				<div className='items'>
+					<i className='fa fa-moon' />
+					<div className='relative flex items-center'>
+						<i className='fa fa-bell' />
+						<span className='absolute top-0 w-3 h-3 bg-red-700 border rounded-full -right-1' />
+					</div>
+				</div>
+				<div className='avatar'>
+					<Image src='http://unsplash.it/g/200?gravity=center' alt='' layout='fill' />
+				</div>
 			</div>
 		</div>
 	);
